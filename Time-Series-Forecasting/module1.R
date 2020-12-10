6756ae658b96be58947c0458f57aeba1fc4f6aa4
phoitack <phoitack@gmail.com>
2020-12-09 18:01:03 -0800

    Added Time Series Forecasting course

---
+++ Time-Series-Forecasting/module1.R
@@ -0,0 +1,158 @@
+# Read the data from Excel into R
+mydata <- read_excel("exercise1.xlsx")
+
+# Look at the first few lines of mydata
+head(mydata)
+
+# Create a ts object called myts
+myts <- ts(data[,2:4], start = c(1981,1), frequency = 4)
+
+
+## -------------------------------------------------------------
+
+# Plot the data with facetting
+autoplot(myts, facets = TRUE)
+
+# Plot the data without facetting
+autoplot(myts, facets = FALSE)
+
+# Plot the three series
+autoplot(gold, facets = FALSE)
+autoplot(woolyrnq, facets = FALSE)
+autoplot(gas, facets = FALSE)
+
+# Find the outlier in the gold series
+goldoutlier <- which.max(gold)
+
+# Look at the seasonal frequencies of the three series
+frequency(gold)
+frequency(woolyrnq)
+frequency(gas)
+
+## -------------------------------------------------------------
+
+
+# Load the fpp2 package
+library(fpp2)
+
+# Create plots of the a10 data
+autoplot(a10)
+ggseasonplot(a10)
+
+# Produce a polar coordinate season plot for the a10 data
+ggseasonplot(a10, polar = TRUE)
+
+# Restrict the ausbeer data to start in 1992
+beer <- window(ausbeer, start=c(1992,1))
+
+# Make plots of the beer data
+autoplot(beer)
+ggsubseriesplot(beer)
+
+
+## -------------------------------------------------------------
+
+# Create an autoplot of the oil data
+autoplot(oil)
+
+# Create a lag plot of the oil data
+gglagplot(oil)
+
+# Create an ACF plot of the oil data
+ggAcf(oil)
+
+
+## -------------------------------------------------------------
+
+# Plot the annual sunspot numbers
+autoplot(sunspot.year)
+ggAcf(sunspot.year)
+
+# Save the lag corresponding to maximum autocorrelation
+maxlag_sunspot <- which.max(sunspot.year)
+
+# Plot the traffic on the Hyndsight blog
+autoplot(hyndsight)
+ggAcf(hyndsight)
+
+# Save the lag corresponding to maximum autocorrelation
+maxlag_hyndsight <- which.max(sunspot.year)
+
+
+
+## -------------------------------------------------------------
+
+# Plot the annual sunspot numbers
+autoplot(sunspot.year)
+ggAcf(sunspot.year)
+
+# Save the lag corresponding to maximum autocorrelation
+maxlag_sunspot <- 1
+
+# Plot the traffic on the Hyndsight blog
+autoplot(hyndsight)
+ggAcf(hyndsight)
+
+# Save the lag corresponding to maximum autocorrelation
+maxlag_hyndsight <- 7
+
+
+
+## -------------------------------------------------------------
+
+# Plot the original series
+autoplot(goog)
+
+# Plot the differenced series
+autoplot(diff(goog))
+
+# ACF of the differenced series
+ggAcf(diff(goog))
+
+# Ljung-Box test of the differenced series
+Box.test(diff(goog), lag = 10, type = "Ljung")
+
+
+## -------------------------------------------------------------
+
+
+# Use naive() to forecast the goog series
+fcgoog <- naive(goog, h = 20)
+
+# Plot and summarize the forecasts
+autoplot(fcgoog)
+summary(fcgoog)
+
+# Use snaive() to forecast the ausbeer series
+fcbeer <- snaive(ausbeer, h = 16)
+
+# Plot and summarize the forecasts
+autoplot(fcbeer)
+summary(fcbeer)
+
+
+## -------------------------------------------------------------
+
+
+
+
+## -------------------------------------------------------------
+
+
+
+
+## -------------------------------------------------------------
+
+
+
+
+
+## -------------------------------------------------------------
+
+
+
+
+
+
+## -------------------------------------------------------------
+
